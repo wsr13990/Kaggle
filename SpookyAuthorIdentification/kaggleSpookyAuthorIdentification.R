b38b9fe4727c0f7467c4c81e79b6b1f89d4a1aa0
unknown <wahyuros@PB90048.telkomsel.co.id>
2017-11-03 16:04:27 +0700

    add text processing for baseline

--- SpookyAuthorIdentification/kaggleSpookyAuthorIdentification.R
+++ SpookyAuthorIdentification/kaggleSpookyAuthorIdentification.R
@@ -1,7 +1,32 @@
+install.packages("SnowballC")
+install.packages("tm")
+
+library(SnowballC)
+library(tm)
+
 setwd("D:/Kaggle/SpookyAuthorIdentification")
 train = read.csv("train.csv")
 test = read.csv("test.csv")
 
 #Baseline performance using BagToWord
-trainText = train$text
-trainLabel = train$author
+
+options(java.parameters = "-Xmx4000m")
+
+convert_count <- function(x){
+  x <- ifelse(x, "Yes", "No")
+}
+
+process_description <- function(data, freq){
+  corpus <- VCorpus(VectorSource(train$text))
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  dtm <- DocumentTermMatrix(corpus)
+  freq_words <- findFreqTerms(dtm, 1)
+  dtm <- dtm[, freq_words]
+  dtm <- apply(dtm, MARGIN=2, convert_count)
+  dtm <- as.matrix(taxlist_dtm)
+  return(dtm)
+}
+
+
